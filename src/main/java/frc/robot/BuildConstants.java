452347be97c931430911fea48078ec2161d1d6e7
dmistry26 <mistrydevshree26@gmail.com>
2026-01-15 22:18:04 -0800

    broken

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC_Programming_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "8558fa6780e11f588188ac8891587b450bc126b3";
-  public static final String GIT_DATE = "2026-01-12 20:28:15 EST";
-  public static final String GIT_BRANCH = "spotless";
-  public static final String BUILD_DATE = "2026-01-14 02:57:14 EST";
-  public static final long BUILD_UNIX_TIME = 1768377434566L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "261a923b7b6d9314baa9d09b2a61d52d0be09e4c";
+  public static final String GIT_DATE = "2026-01-14 18:29:20 EST";
+  public static final String GIT_BRANCH = "shooter";
+  public static final String BUILD_DATE = "2026-01-14 18:57:25 EST";
+  public static final long BUILD_UNIX_TIME = 1768435045107L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+
+public class ShooterIOSim extends ShooterIOTalonFX { 
+      private final FlywheelSim leftFlywheelSimModel = 
+      new FlywheelSim(x
+        
+
