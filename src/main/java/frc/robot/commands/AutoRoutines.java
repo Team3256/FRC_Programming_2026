f3aa5963747b44fc9b40d189d3a088c9621e3c2d
Samuel Yu <116925676+memori034@users.noreply.github.com>
2026-01-17 13:42:12 -0800

    basic forward

--- src/main/java/frc/robot/commands/AutoRoutines.java
+++ src/main/java/frc/robot/commands/AutoRoutines.java
@@ -1,4 +1,44 @@
+// Copyright (c) 2025 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by a 
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 package frc.robot.commands;
 
+import choreo.auto.AutoFactory;
+import choreo.auto.AutoRoutine;
+import choreo.auto.AutoTrajectory;
+import frc.robot.subsystems.swerve.CommandSwerveDrivetrain;
+
 public class AutoRoutines {
+
+  private final AutoFactory m_factory;
+
+  // subsystems
+
+  private final AutoCommands m_autoCommands;
+
+  private final CommandSwerveDrivetrain m_drivetrain;
+
+  public AutoRoutines(
+      AutoFactory factory, AutoCommands mAutoCommands, CommandSwerveDrivetrain drivetrain) {
+    m_factory = factory;
+    m_autoCommands = mAutoCommands;
+    m_drivetrain = drivetrain; // subsystems
+  }
+
+  public AutoRoutine forward() {
+    final AutoRoutine routine = m_factory.newRoutine("forward");
+    final AutoTrajectory forward = routine.trajectory("forward");
+    routine.active().onTrue(forward.resetOdometry().andThen(forward().cmd()));
+    return routine;
+  }
+
+  private static class AutoCommands {
+
+    public void AutoCommands() { // void for now, fill subsystems
+    }
+  }
 }
