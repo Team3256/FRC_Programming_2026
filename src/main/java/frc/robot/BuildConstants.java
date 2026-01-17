ba2150599936f59a83a614f939fc44c9cdcf1562
Samuel Yu <116925676+memori034@users.noreply.github.com>
2026-01-17 14:16:24 -0800

    indexer

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC_Programming_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 43;
-  public static final String GIT_SHA = "db37439ac3db61d244f9e10e089e1978ee168a48";
-  public static final String GIT_DATE = "2026-01-16 21:29:48 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-16 21:42:52 EST";
-  public static final long BUILD_UNIX_TIME = 1768617772507L;
+  public static final int GIT_REVISION = 47;
+  public static final String GIT_SHA = "37463ac2ad0c4c22a1ab89e2251b1213efb1ff7c";
+  public static final String GIT_DATE = "2026-01-17 16:47:03 EST";
+  public static final String GIT_BRANCH = "indexer";
+  public static final String BUILD_DATE = "2026-01-17 17:16:09 EST";
+  public static final long BUILD_UNIX_TIME = 1768688169410L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
