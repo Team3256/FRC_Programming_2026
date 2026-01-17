a6cb4d79382666b876a6d26affe1eef246f837e7
Samuel Yu <116925676+memori034@users.noreply.github.com>
2026-01-17 12:10:13 -0800

    merge

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC_Programming_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "450439a4e29d06b179dcfafe5177e9b1ace74dda";
-  public static final String GIT_DATE = "2026-01-15 18:04:16 EST";
-  public static final String GIT_BRANCH = "intakerollers";
-  public static final String BUILD_DATE = "2026-01-15 18:27:16 EST";
-  public static final long BUILD_UNIX_TIME = 1768519636102L;
+  public static final int GIT_REVISION = 18;
+  public static final String GIT_SHA = "c0d4916afefea26a37427b977a8e7d2cd63d43ed";
+  public static final String GIT_DATE = "2026-01-16 02:29:44 EST";
+  public static final String GIT_BRANCH = "feat/indexer";
+  public static final String BUILD_DATE = "2026-01-17 15:06:26 EST";
+  public static final long BUILD_UNIX_TIME = 1768680386494L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
---
+++ src/main/java/frc/robot/subsystems/indexer/IndexerIOTalonFX.java
@@ -0,0 +1,77 @@
+// Copyright (c) 2025 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by a 
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot.subsystems.indexer;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+import frc.robot.utils.PhoenixUtil;
+
+public class IndexerIOTalonFX implements IndexerIO {
+  private final TalonFX intakeMotor =
+      new TalonFX(IndexerConstants.kIndexerMotorID, "bruh"); // TODO: remove deprecated
+  final VelocityVoltage intakeRequest = new VelocityVoltage(0).withSlot(0);
+
+  private final StatusSignal<Voltage> IndexerMotorVoltage = intakeMotor.getMotorVoltage();
+  private final StatusSignal<AngularVelocity> IndexerMotorVelocity = intakeMotor.getVelocity();
+  private final StatusSignal<Current> IndexerMotorStatorCurrent = intakeMotor.getStatorCurrent();
+  private final StatusSignal<Current> IndexerMotorSupplyCurrent = intakeMotor.getSupplyCurrent();
+  private final StatusSignal<Temperature> IndexerMotorTemperature = intakeMotor.getDeviceTemp();
+
+  public IndexerIOTalonFX() {
+    PhoenixUtil.applyMotorConfigs(
+        intakeMotor, IndexerConstants.motorConfigs, IndexerConstants.flashConfigRetries);
+
+    BaseStatusSignal.setUpdateFrequencyForAll(
+        IndexerConstants.updateFrequency,
+        IndexerMotorVoltage,
+        IndexerMotorVelocity,
+        IndexerMotorStatorCurrent,
+        IndexerMotorSupplyCurrent,
+        IndexerMotorTemperature);
+    PhoenixUtil.registerSignals(
+        true,
+        IndexerMotorVoltage,
+        IndexerMotorVelocity,
+        IndexerMotorStatorCurrent,
+        IndexerMotorSupplyCurrent,
+        IndexerMotorTemperature);
+    intakeMotor.optimizeBusUtilization();
+  }
+
+  public void updateInputs(IndexerIOInputs inputs) {
+
+    inputs.indexerMotorVoltage = IndexerMotorVoltage.getValueAsDouble();
+    inputs.indexerMotorVelocity = IndexerMotorVelocity.getValueAsDouble();
+    inputs.indexerMotorStatorCurrent = IndexerMotorStatorCurrent.getValueAsDouble();
+    inputs.indexerMotorSupplyCurrent = IndexerMotorSupplyCurrent.getValueAsDouble();
+    inputs.indexerMotorTemperature = IndexerMotorTemperature.getValueAsDouble();
+  }
+
+  public void setVoltage(double voltage) {
+    intakeMotor.setVoltage(voltage);
+  }
+
+  public void setVelocity(double velocity) {
+    intakeMotor.setControl(intakeRequest.withVelocity(velocity));
+  }
+
+  public void off() {
+    intakeMotor.setControl(new NeutralOut());
+  }
+
+  public TalonFX getIndexerMotor() {
+    return intakeMotor;
+  }
+}
