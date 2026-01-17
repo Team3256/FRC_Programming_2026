cf61b6f957eaa35544fbe275aecdffcad86e4fdf
Grace Xu <71201761+gxu0904@users.noreply.github.com>
2026-01-17 13:42:17 -0800

    eh

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC_Programming_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 43;
-  public static final String GIT_SHA = "db37439ac3db61d244f9e10e089e1978ee168a48";
-  public static final String GIT_DATE = "2026-01-16 21:29:48 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-16 21:42:52 EST";
-  public static final long BUILD_UNIX_TIME = 1768617772507L;
+  public static final int GIT_REVISION = 47;
+  public static final String GIT_SHA = "d523bea36f4c69894f5aa686e64ee5ee544f891d";
+  public static final String GIT_DATE = "2026-01-17 15:37:42 EST";
+  public static final String GIT_BRANCH = "sotm";
+  public static final String BUILD_DATE = "2026-01-17 16:37:52 EST";
+  public static final long BUILD_UNIX_TIME = 1768685872367L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
