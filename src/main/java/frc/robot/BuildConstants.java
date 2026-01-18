590cc5e8c95ee07015eed2b67b0f4af1fbe509c8
Grace Xu <71201761+gxu0904@users.noreply.github.com>
2026-01-17 16:12:44 -0800

    hopefully we can sotm

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC_Programming_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 57;
-  public static final String GIT_SHA = "92184d1379a62a38358ea4963104084216b6bcc9";
-  public static final String GIT_DATE = "2026-01-17 18:05:16 EST";
+  public static final int GIT_REVISION = 68;
+  public static final String GIT_SHA = "8ead23641e244188379e6afc766ca3b3a90606f1";
+  public static final String GIT_DATE = "2026-01-17 18:53:03 EST";
   public static final String GIT_BRANCH = "sotm";
-  public static final String BUILD_DATE = "2026-01-17 18:25:57 EST";
-  public static final long BUILD_UNIX_TIME = 1768692357235L;
+  public static final String BUILD_DATE = "2026-01-17 19:09:15 EST";
+  public static final long BUILD_UNIX_TIME = 1768694955240L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
