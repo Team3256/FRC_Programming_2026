c7474f27166b074530f04d6c85a435fab1d721cb
dmistry26 <mistrydevshree26@gmail.com>
2026-01-19 16:43:29 -0800

    constants w updated hubids

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC_Programming_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 62;
-  public static final String GIT_SHA = "2346b53506c1b36e84dbc200fdcdb256621eb52e";
-  public static final String GIT_DATE = "2026-01-17 18:09:00 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-17 18:09:16 EST";
-  public static final long BUILD_UNIX_TIME = 1768691356430L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 63;
+  public static final String GIT_SHA = "ac04eabdf7f4ba1baf3382a1647e05660f7b7f52";
+  public static final String GIT_DATE = "2026-01-17 18:09:25 EST";
+  public static final String GIT_BRANCH = "vision";
+  public static final String BUILD_DATE = "2026-01-19 18:32:11 EST";
+  public static final long BUILD_UNIX_TIME = 1768865531380L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
