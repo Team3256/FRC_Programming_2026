d308a329b2f27d9787cf0721073af8954642d8ca
dmistry26 <mistrydevshree26@gmail.com>
2026-02-14 15:27:49 -0800

    spotless

--- src/main/java/frc/robot/subsystems/turret/TurretConstants.java
+++ src/main/java/frc/robot/subsystems/turret/TurretConstants.java
@@ -39,7 +39,8 @@ public class TurretConstants {
 
   public static final Rotation2d turretOffset = Rotation2d.kPi;
 
-  public static final Transform2d driveBaseToTurret = new Transform2d(Units.inchesToMeters(6.750), Units.inchesToMeters(-6.750), Rotation2d.kZero);
+  public static final Transform2d driveBaseToTurret =
+      new Transform2d(Units.inchesToMeters(6.750), Units.inchesToMeters(-6.750), Rotation2d.kZero);
 
   public static final CANcoderConfiguration cancoderConfiguration1 =
       new CANcoderConfiguration()
@@ -92,7 +93,7 @@ public class TurretConstants {
   // CRT constants
   public static final int drivingGear1 = 90;
   public static final int drivenGear1 = 10;
-  public static final int drivingGear2 = 90 / 10 * 32 ;
+  public static final int drivingGear2 = 90 / 10 * 32;
   public static final int drivenGear2 = 56;
 
   public static final double ratio1 = (double) drivingGear1 / drivenGear1;
