aefe33f95e0e62d66af09fd603cd8da7d8c15109
ella <jellyella321@gmail.com>
2026-02-16 12:48:04 -0800

    depotClimb + depotNoClimb

--- src/main/java/frc/robot/commands/AutoRoutines.java
+++ src/main/java/frc/robot/commands/AutoRoutines.java
@@ -10,7 +10,9 @@ package frc.robot.commands;
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.Superstructure;
+import frc.robot.subsystems.Superstructure.StructureState;
 import frc.robot.subsystems.swerve.CommandSwerveDrivetrain;
 
 public class AutoRoutines {
@@ -44,11 +46,55 @@ public class AutoRoutines {
 
   public AutoRoutine depotNoClimb() {
     final AutoRoutine routine = m_factory.newRoutine("depotOutpost"); // no climb
-    final AutoTrajectory depot = routine.trajectory("depotClimb");
+    final AutoTrajectory depot = routine.trajectory("depot");
     routine
         .active()
         .onTrue(depot.resetOdometry().andThen(Commands.waitSeconds(2)).andThen(depot.cmd()));
-    // depot.atTimeBeforeEnd(0.5).onTrue(m_superstructure)
+    depot
+        .atTime("Start")
+        .onTrue(
+            m_superstructure
+                .setState(StructureState.REV)
+                .andThen(Commands.waitSeconds(2))
+                .andThen(m_superstructure.setState(StructureState.SHOOT)));
+    depot.atTime("Intake").onTrue(m_superstructure.setState(StructureState.INTAKE));
+    depot
+        .atTime("Shoot")
+        .onTrue(
+            m_superstructure
+                .setState(StructureState.REV)
+                .andThen(Commands.waitSeconds(2))
+                .andThen(m_superstructure.setState(StructureState.SHOOT)));
+    depot.atTime("Shoot2").onTrue(m_superstructure.setState(StructureState.SHOOT));
+
+    return routine;
+  }
+
+  public AutoRoutine depotClimb() {
+    final AutoRoutine routine = m_factory.newRoutine("depotOutpostClimb");
+    final AutoTrajectory depotClimb = routine.trajectory("depotClimb");
+    routine
+        .active()
+        .onTrue(
+            depotClimb.resetOdometry().andThen(Commands.waitSeconds(2)).andThen(depotClimb.cmd()));
+    depotClimb
+        .atTime("Start")
+        .onTrue(
+            m_superstructure
+                .setState(StructureState.REV)
+                .andThen(Commands.waitSeconds(2))
+                .andThen(m_superstructure.setState(StructureState.SHOOT)));
+    depotClimb.atTime("Intake").onTrue(m_superstructure.setState(StructureState.INTAKE));
+    depotClimb
+        .atTime("Shoot")
+        .onTrue(
+            m_superstructure
+                .setState(StructureState.REV)
+                .andThen(Commands.waitSeconds(2))
+                .andThen(m_superstructure.setState(StructureState.SHOOT)));
+    depotClimb.atTime("Shoot2").onTrue(m_superstructure.setState(StructureState.SHOOT));
+    depotClimb.atTime("Climb").onTrue(m_superstructure.setState(StructureState.CLIMB));
+
     return routine;
   }
 
