95cf56d637530251c927ef4bfd3697e572d9c875
dmistry26 <mistrydevshree26@gmail.com>
2026-03-01 11:51:28 -0800

    brah

--- src/main/java/frc/robot/subsystems/led/LedConstants.java
+++ src/main/java/frc/robot/subsystems/led/LedConstants.java
@@ -1,3 +1,10 @@
+// Copyright (c) 2025 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by a 
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 package frc.robot.subsystems.led;
 
 import com.ctre.phoenix6.configs.CANdleConfiguration;
@@ -5,10 +12,8 @@ import com.ctre.phoenix6.configs.LEDConfigs;
 import com.ctre.phoenix6.signals.StripTypeValue;
 
 public final class LedConstants {
-    public static final CANdleConfiguration candleConfig =
-            new CANdleConfiguration().withLED(
-                    new LEDConfigs().withStripType(StripTypeValue.RGB)
-            );
+  public static final CANdleConfiguration candleConfig =
+      new CANdleConfiguration().withLED(new LEDConfigs().withStripType(StripTypeValue.RGB));
 
-    public static final int stripLength = 100;
+  public static final int stripLength = 100;
 }
