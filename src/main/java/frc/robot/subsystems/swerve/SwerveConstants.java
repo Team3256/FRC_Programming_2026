2acba72f95fc493f38a49c8592949c4807de085f
dmistry26 <mistrydevshree26@gmail.com>
2026-03-01 12:05:34 -0800

    diagonal

--- src/main/java/frc/robot/subsystems/swerve/SwerveConstants.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveConstants.java
@@ -52,9 +52,11 @@ public final class SwerveConstants {
   public static final class BumpTargets {
 
     public static Pose2d BOTTOM_BUMP =
-        new Pose2d(3.4559648036956787, 2.4935503005981445, Rotation2d.fromDegrees(0));
+        new Pose2d(
+            3.4559648036956787, 2.4935503005981445, Rotation2d.fromDegrees(45.744063302312063968));
 
     public static Pose2d TOP_BUMP =
-        new Pose2d(3.4064114093780518, 5.554626941680908, Rotation2d.fromDegrees(0));
+        new Pose2d(
+            3.4064114093780518, 5.554626941680908, Rotation2d.fromDegrees(45.744063302312063968));
   }
 }
