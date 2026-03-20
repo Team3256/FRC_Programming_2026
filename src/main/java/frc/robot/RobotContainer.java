cca9edf14cfa9ee830e0bfa738104f322ba527e4
dmistry26 <mistrydevshree26@gmail.com>
2026-03-19 17:11:18 -0700

    fix

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,7 +15,6 @@ import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -113,7 +112,6 @@ public class RobotContainer {
           () -> drivetrain.getState().Pose,
           drivetrain::getFieldRelativeSpeeds,
           TurretConstants.driveBaseToTurret);
-
 
   private final Led led = new Led();
 
